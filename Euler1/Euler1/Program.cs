c24e3483723952426196dd4416bb82c6c44051f1
akhramon <akhramon@gmail.com>
2016-02-24 14:59:08 +0300

    Euler 1
    задача

---
+++ Euler1/Euler1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class Program
+    {
+        public static void Main()
+        { 
+        
+        }
+
+        public List<int> SearchMultiple()
+        {
+
+            List<int> l = t;
+
+            for (int c = 1; c <= 1000; c++)
+            {
+                Console.WriteLine(c);
+                
+
+                
+
+
+                List<int> t = new List<int>(); 
+
+
+                int f = ((c % 3 == 0) ? c : 0);
+                t.Add(f);
+                int g = ((c % 3 == 0) ? c : 0);
+                t.Add(g);
+
+
+                return t;
+            }
+            return l;
+        }
+        public int Summa(List<int> i)
+        {
+            
+            int h = i.Sum();
+            Console.WriteLine(h);
+
+
+
+
+
+                
+           
+            return h;
+            
+        }
+
+    }
+}
